f35ff401f45ddc6ef2db12f87a21c5ac19bda2e5
Mariam Bouzid <mariam.bouzid@icloud.com>
2016-02-05 16:09:13 +0100

    Update FileAdapter.java

--- src/main/java/com/univangers/l3info/mbouzid/file_explorer/FileAdapter.java
+++ src/main/java/com/univangers/l3info/mbouzid/file_explorer/FileAdapter.java
@@ -1,18 +1,29 @@
 package com.univangers.l3info.mbouzid.fileexplorer;
 
 import android.content.Context;
-import android.widget.ArrayAdapter;
-import java.io.File;
+
 import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.view.View;
+
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.io.File;
 
 public class FileExplorer extends ArrayAdapter<File>
 {
 
-  public FileAdapter(Context context, File [] objects)
+  File m_root ;
+  
+  public FileAdapter(Context context, File root)
   {
-     super(context, R.layout.line, objects);
+     super(context, R.layout.line,new ArrayList<File>(Arrays.asList(root.listFiles())));
+     m_root=root;
   }
   
   public View getView(int position, View v, ViewGroup parent)
@@ -21,9 +32,23 @@ public class FileExplorer extends ArrayAdapter<File>
     {
       LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
       v = inflater.inflate(R.layout.line,parent,false);
-      TextView cdir=(TextView)v.findViewById(R.id.m_directory);
-      cdir.setText(getItem(position).getName());
+      
     }
+    File item = getItem(position);
+    ImageView img = (ImageView)v.findViewById(R.id.icon_image);
+    
+    if (item.isDirectory())
+    {
+      img.setImageResource(R.drawable.directory_icon);
+    }
+    else
+    {
+      img.setImageResource(R.drawable.blank_file_icon);
+    }
+      
+    TextView current_directory=(TextView)v.findViewById(R.id.path_name);
+    current_directory.setText(getItem(position).getName());
+    
     return v;
  }
 
